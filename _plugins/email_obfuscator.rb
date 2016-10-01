1168de86a076901cfdcd104843130256628e738c
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2016-10-01 11:12:27 +0200

    First draft, still not tested in Jekyll

--- _plugins/email_obfuscator.rb
+++ _plugins/email_obfuscator.rb
@@ -16,3 +16,55 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =end
+
+module Jekyll
+    class EmailTag < Liquid::Tag
+
+        SPAN_CLASS = "3m41l"
+
+        AT = 'varaiRi8'
+        DOT = 'chie6Aet'
+        DASH = 'oaPoo7eo'
+        UNDERSCORE = 'Oe8eed6M'
+
+        MAILTO = '&#109;ai&#108;to&#58;'
+
+        private
+        def saltify(s)
+            "&#123;" + s + "&#126;"
+        end
+
+        private
+        def encode(s)
+            s = s.gsub("@", saltify(AT))
+            s = s.gsub(".", saltify(DOT))
+            s = s.gsub("-", saltify(DASH))
+            s = s.gsub("_", saltify(UNDERSCORE))
+        end
+
+        def render(context)
+            values = INPUT.split("@")
+            data_user = values[0]
+            data_domain = values[1]
+            obfuscated = encode(INPUT)
+
+            at = saltify(AT)
+            dot = saltify(DOT)
+            underscore = saltify(UNDERSCORE)
+            dash = saltify(DASH)
+
+            "<a href=\"#{MAILTO}#{obfuscated}\" " +
+                "onmouseover=\"this.href=this.href" +
+                    ".replace(/#{at}/,'&#64;')" +
+                    ".replace(/#{dot}/g,'&#46;')" +
+                    ".replace(/#{underscore}/g,'&#95;')" +
+                    ".replace(/#{dash}/g,'&#45;')\">" +
+            "<span class=\"#{SPAN_CLASS}\" " +
+                "data-x=\"#{data_user}\" " +
+                "data-y=\"#{data_domain}\"></span>" +
+            "</a>"
+        end
+    end
+end
+
+Liquid::Template.register_tag('email', Jekyll::EmailTag)
